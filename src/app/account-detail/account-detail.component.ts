2a4ca623a343df63ba68a5eb2a7b3381f3fb67f8
Leo-paul <leopaulmartin@hotmail.fr>
2021-10-24 00:58:10 +0200

    Subscribing to Route params

--- src/app/account-detail/account-detail.component.ts
+++ src/app/account-detail/account-detail.component.ts
@@ -1,10 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, ParamMap } from '@angular/router';
 
 @Component({
   selector: 'app-account-detail',
   template: ` <p>account-detail works!</p>
-    <p>Account ID: {{ id }}</p>`,
+    <p>Account ID: {{ id }}</p>
+
+    <nav>
+      <ul>
+        <li><a [routerLink]="['/account', 1]">Account 1</a></li>
+        <li><a [routerLink]="['/account', 2]">Account 2</a></li>
+        <li><a [routerLink]="['/account', 3]">Account 3</a></li>
+      </ul>
+    </nav>`,
   styles: [],
 })
 export class AccountDetailComponent implements OnInit {
@@ -12,6 +20,9 @@ export class AccountDetailComponent implements OnInit {
   constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
-    this.id = ''+this.route.snapshot.paramMap.get('id');
+    //this.id = ''+this.route.snapshot.paramMap.get('id');
+    this.route.paramMap.subscribe((params: ParamMap) => {
+      this.id = '' + params.get('id');
+    });
   }
 }
--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -6,14 +6,6 @@ import { Component, OnInit } from '@angular/core';
     <p>
       account works!
     </p>
-
-    <nav>
-      <ul>
-        <li><a [routerLink]="['/account', 1]">Account 1</a></li>
-        <li><a [routerLink]="['/account', 2]">Account 2</a></li>
-        <li><a [routerLink]="['/account', 3]">Account 3</a></li>
-      </ul>
-    </nav>
   `,
   styles: [
   ]
